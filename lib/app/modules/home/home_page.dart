fce4913687c0e31390a505f568fb0c55d6d6187e
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2022-09-12 15:15:56 -0300

    Default flutter project created, Modular installation and Rango librarie Initially a default project was created, after with Slidy, installed Modular with Mobx and a package project Rango created in the libraries directory

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:rango/rango.dart';
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = 'Home'}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final HomeStore store;
+
+  @override
+  void initState() {
+    super.initState();
+    store = Modular.get<HomeStore>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Observer(
+        builder: (context) => Text('${store.counter}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
