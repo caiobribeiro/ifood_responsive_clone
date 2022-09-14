a9acc6d543c20ed96ba878d1881a588223e561bf
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2022-09-14 14:15:22 -0300

    Category list widget implementation within Rango library and redered in home

--- lib/app/modules/home/widgets/all_stores/all_store_widget.dart
+++ lib/app/modules/home/widgets/all_stores/all_store_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rango/rango.dart';
 
 class AllStoresWidget extends StatefulWidget {
-  const AllStoresWidget({super.key});
+  final TabController tabController;
+  const AllStoresWidget({super.key, required this.tabController});
 
   @override
   State<AllStoresWidget> createState() => _AllStoresWidgetState();
@@ -10,9 +12,62 @@ class AllStoresWidget extends StatefulWidget {
 class _AllStoresWidgetState extends State<AllStoresWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
-      child: const Center(child: Text("Início")),
+    return Column(
+      children: [
+        SizedBox(
+          height: 250,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            children: [
+              CategoryListWidget(
+                items: [
+                  CategoryListItem(
+                    label: 'Restaurantes',
+                    imageUrl:
+                        'https://i.pinimg.com/originals/6a/27/ac/6a27ac5560df47ec8bb998051b74b1f0.png',
+                    onTap: (item) {
+                      widget.tabController.animateTo(1);
+                    },
+                  ),
+                  CategoryListItem(
+                    label: 'Mercados',
+                    imageUrl:
+                        'https://cdn-icons-png.flaticon.com/512/3082/3082011.png',
+                    onTap: (item) {
+                      widget.tabController.animateTo(2);
+                    },
+                  ),
+                  CategoryListItem(
+                    label: 'Bebidas',
+                    imageUrl:
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWHNMcRK_crxVO7kxj9tiKcICDOCuHyIoe4SEHjDzYaDNkGpsOosFbjhJbBfTVhrpt1K0&usqp=CAU',
+                    onTap: (item) {
+                      widget.tabController.animateTo(3);
+                    },
+                  ),
+                  CategoryListItem(
+                    label: 'Drogaria',
+                    imageUrl:
+                        'https://cdn-icons-png.flaticon.com/512/4320/4320337.png',
+                    onTap: (item) {
+                      widget.tabController.animateTo(4);
+                    },
+                  ),
+                  CategoryListItem(
+                    label: 'Exemplo',
+                    imageUrl:
+                        'https://cdn-icons-png.flaticon.com/512/4320/4320337.png',
+                    onTap: (item) {
+                      widget.tabController.animateTo(4);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
